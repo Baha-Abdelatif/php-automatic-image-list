0d2a536193c06ac8ebcf55f03363538f5ed60817
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-06 18:01:21 +0200

    Ensure data validation and refine styling.

--- index.php
+++ index.php
@@ -10,17 +10,22 @@ $images = [];
 
 $handle = opendir(__DIR__ . '/images');
 while (($currentFile = readdir($handle)) !== false) {
+  $imgTitle = "A beautiful place.";
+  $imgDescription = ["Cannot found more infos about this place for now. Please try later or refresh the page."];
   if ($currentFile === "." || $currentFile === ".." || $currentFile === ".DS_Store") continue;
   $extension = pathinfo($currentFile, PATHINFO_EXTENSION);
   if (!in_array($extension, $ALLOWED_EXTENSIONS)) continue;
-  $textfilename = str_ireplace($extension, "txt", $currentFile);
+  // $textfilename = str_ireplace($extension, "txt", $currentFile);
+  // or
+  $textfilename = pathinfo($currentFile, PATHINFO_FILENAME) . ".txt";
+  if (!file_exists(__DIR__ . "/images/$textfilename")) continue;
   $text = file_get_contents(__DIR__ . "/images/$textfilename");
   $titleIdx = stripos($text, "\n", 0);
   $imgTitle = substr($text, 0, $titleIdx);
   $imgDescription = substr($text, $titleIdx);
   $images[$imgTitle] = [
     "location" => $currentFile,
-    "description" => $imgDescription,
+    "description" => explode("\n", $imgDescription),
   ];
 }
 closedir($handle);
@@ -30,7 +35,9 @@ closedir($handle);
 <?php foreach ($images as $title => $image) : ?>
   <h3><?php e($title) ?></h3>
   <img src="images/<?php echo rawurlencode($image["location"]); ?>" alt="an interesting point of view">
-  <p><?php e($image['description']) ?></p>
+  <?php foreach ($image['description'] as $description) : ?>
+    <p><?php e($description) ?></p>
+  <?php endforeach; ?>
 <?php endforeach; ?>
 
 <?php require_once __DIR__ . "/views/footer.html" ?>
