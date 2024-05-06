773e6dafc3d148ebca9516058e7ae344b86a696a
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-06 17:25:01 +0200

    Fetch images infos in each .txt file and display image title and description.

--- index.php
+++ index.php
@@ -1,22 +1,36 @@
 <?php require_once __DIR__ . "/views/header.html";
 require_once __DIR__ . "/inc/functions.php";
 
-$handle = opendir(__DIR__ . '/images');
+$ALLOWED_EXTENSIONS = [
+  "jpg",
+  "png",
+  "jpeg",
+];
 $images = [];
 
+$handle = opendir(__DIR__ . '/images');
 while (($currentFile = readdir($handle)) !== false) {
   if ($currentFile === "." || $currentFile === ".." || $currentFile === ".DS_Store") continue;
   $extension = pathinfo($currentFile, PATHINFO_EXTENSION);
-  if ($extension !== "jpg" && $extension !== "jpeg" && $extension !== "png") continue;
-  $images[] = $currentFile;
+  if (!in_array($extension, $ALLOWED_EXTENSIONS)) continue;
+  $textfilename = str_ireplace($extension, "txt", $currentFile);
+  $text = file_get_contents(__DIR__ . "/images/$textfilename");
+  $titleIdx = stripos($text, "\n", 0);
+  $imgTitle = substr($text, 0, $titleIdx);
+  $imgDescription = substr($text, $titleIdx);
+  $images[$imgTitle] = [
+    "location" => $currentFile,
+    "description" => $imgDescription,
+  ];
 }
 closedir($handle);
+
 ?>
 
-<ul>
-  <?php foreach ($images as $image) : ?>
-    <li><img src="images/<?php echo rawurlencode($image); ?>" alt="an interesting point of view"></li>
-  <?php endforeach; ?>
-</ul>
+<?php foreach ($images as $title => $image) : ?>
+  <h3><?php e($title) ?></h3>
+  <img src="images/<?php echo rawurlencode($image["location"]); ?>" alt="an interesting point of view">
+  <p><?php e($image['description']) ?></p>
+<?php endforeach; ?>
 
 <?php require_once __DIR__ . "/views/footer.html" ?>
